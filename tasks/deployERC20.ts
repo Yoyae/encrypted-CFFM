f4108d325ec55c548a519647046bde38d79bf3a8
Yoyae <gingin.fr@gmail.com>
2023-10-05 11:11:43 +0200

    Fix deploy function of eERC20 to add constructor parameters

--- tasks/deployERC20.ts
+++ tasks/deployERC20.ts
@@ -6,7 +6,7 @@ import { getSigners } from "../test/signers";
 task("task:deployERC20").setAction(async function (taskArguments: TaskArguments, { ethers }) {
   const signers = await getSigners(ethers);
   const erc20Factory = await ethers.getContractFactory("EncryptedERC20");
-  const encryptedERC20 = await erc20Factory.connect(signers.alice).deploy();
+  const encryptedERC20 = await erc20Factory.connect(signers.alice).deploy("Naraggara", "NARA");
   await encryptedERC20.waitForDeployment();
   console.log("EncryptedERC20 deployed to: ", await encryptedERC20.getAddress());
 });
